8d204a59f165781b1d048ea5f9ca5dc9958c3b9d
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-25 15:10:13 -0600

    pagination

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
+    before_action :set_params, only: :index
 
     def index
-        students = Student.all
+        students = Student.filter(@filters).page(@page).per(@per_page)
         render json: students
     end
+
+private
+    def set_params
+        @filters = params[:filter]
+        @page = params[:page] || 1
+        @per_page = params[:per]
+    end
+    
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,16 @@
 class Student < ApplicationRecord
     has_many :lectures, through: :student_lectures
+    # def SetFilter(filter)
+        scope :age_ascending, -> {order(age: :desc)}
+    # end
+    # scope :age_ascending, -> {order(age: :asc)}
+    def self.filter(filter)
+        if filter == 'age_desc'
+        self.order(age: :desc)
+        elsif filter == 'age_asc'
+        self.order(age: :asc)
+        else
+        self.order(id: :asc)
+        end
+    end
 end
