9e637b5e7c7d681a7e81367fad62098d31870f28
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-26 16:04:06 -0600

    jj

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,5 @@
 class Student < ApplicationRecord
     has_many :lectures, through: :student_lectures
-    # def SetFilter(filter)
-        scope :age_ascending, -> {order(age: :desc)}
-    # end
-    # scope :age_ascending, -> {order(age: :asc)}
     def self.filter(filter)
         if filter == 'age_desc'
         self.order(age: :desc)
