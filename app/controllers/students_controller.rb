10c4e371e06d4d70b975ef0aeddc639902f5b209
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-01 10:51:52 -0600

    show method on student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,14 @@
 class StudentsController < ApplicationController
     before_action :set_params, only: :index
+    before_action :set_student, only: :show
 
     def index
         students = Student.filter(@filters).page(@page).per(@per_page)
         render json: students
+    end
+
+    def show
+        render json: @student
     end
 
 private
@@ -11,6 +16,10 @@ private
         @filters = params[:filter]
         @page = params[:page] || 1
         @per_page = params[:per]
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
     end
     
 end
