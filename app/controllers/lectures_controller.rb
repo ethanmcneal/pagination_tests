0fa414a14f40c7a287d60ffd59880a5a6e3df612
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-03 12:07:45 -0600

    lecture index

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,2 +1,15 @@
 class LecturesController < ApplicationController
+
+    before_action set_params, only: :index
+    def index
+        lectures = Lecture.filter(@filters).page(@page).per(@per_page)
+        render json: lectures
+    end
+
+    private
+    
+    def set_params
+        @page = params[:page] || 1
+        @per_page = params[:per_page] || 5 
+    end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,13 @@
 class Lecture < ApplicationRecord
     has_many :students, through: :student_lectures
+
+    def self.filter(filter)
+        if filter == 'age_desc'
+        self.order(age: :desc)
+        elsif filter == 'age_asc'
+        self.order(age: :asc)
+        else
+        self.order(id: :asc)
+        end
+    end
 end
